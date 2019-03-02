8ba105121b14406aa818e2e9b481def3a3512fb4
sahil <sahilmangotra1997@gmail.com>
2019-03-02 21:42:35 +0530

    indentation error in socket from last commit

--- bot/consumer.py
+++ bot/consumer.py
@@ -23,34 +23,34 @@ class LogConsumer(AsyncWebsocketConsumer):
             await self.accept()
 
 
-async def disconnect(self, close_code):
-    # Leave room group
-    await self.channel_layer.group_discard(
-        self.room_group_name,
-        self.channel_name
-    )
+    async def disconnect(self, close_code):
+        # Leave room group
+        await self.channel_layer.group_discard(
+            self.room_group_name,
+            self.channel_name
+        )
 
 
-# Receive message from WebSocket
-async def receive(self, text_data):
-    text_data_json = json.loads(text_data)
-    message = text_data_json['message']
+    # Receive message from WebSocket
+    async def receive(self, text_data):
+        text_data_json = json.loads(text_data)
+        message = text_data_json['message']
 
-    # Send message to room group
-    await self.channel_layer.group_send(
-        self.room_group_name,
-        {
-            'type': 'log_message',
+        # Send message to room group
+        await self.channel_layer.group_send(
+            self.room_group_name,
+            {
+                'type': 'log_message',
+                'message': message
+            }
+        )
+
+
+    # Receive message from room group
+    async def log_message(self, event):
+        message = event['message']
+
+        # Send message to WebSocket
+        await self.send(text_data=json.dumps({
             'message': message
-        }
-    )
-
-
-# Receive message from room group
-async def log_message(self, event):
-    message = event['message']
-
-    # Send message to WebSocket
-    await self.send(text_data=json.dumps({
-        'message': message
-    }))
+        }))
